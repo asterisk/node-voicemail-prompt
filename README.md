baab3c1681a51387bccc9d2d0d5a7351a62c9aa5
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2014-09-27 10:56:06 -0500

    Prompt module for Asterisk voicemail.

--- README.md
+++ README.md
@@ -20,6 +20,41 @@ or add the following the your package.json file
 
 # Usage
 
+Create a prompt object from a sequence of sounds and a channel
+
+```JavaScript
+var promptHelper = require('voicemail-prompt');
+var sounds = [{
+  sound: 'sound:hello-world',
+  skipable: false, // can this stop be stopped during playback?
+  postSilence: 1 // in seconds
+}];
+
+var prompt = promptHelper.create(sounds, channel);
+```
+
+Initiate playback of all sounds in sequence:
+
+```JavaScript
+prompt.play()
+  .then(function(played) {
+    // played will be true if all sounds completed
+  })
+  .catch(function(err) {
+  })
+  .done();
+```
+
+Stop the prompt at any point in the sequence:
+
+```JavaScript
+prompt.stop();
+```
+
+Calling stop while the sequence of sounds is still playing will return false from the original promise returned by the play operation.
+
+If the channel hangs up while the sequence of sounds is still playing, the promise returned by the play operation will return an error that can be caught with the catch method of the promise.
+
 # Development
 
 After cloning the git repository, run the following to install the module and all dev dependencies:
---
+++ test/tests.js
@@ -0,0 +1,214 @@
+/**
+ * Prompt module unit tests.
+ *
+ * @module tests-context
+ * @copyright 2014, Digium, Inc.
+ * @license Apache License, Version 2.0
+ * @author Samuel Fortier-Galarneau <sgalarneau@digium.com>
+ */
+
+'use strict';
+
+/*global describe:false*/
+/*global beforeEach:false*/
+/*global afterEach:false*/
+/*global it:false*/
+
+var mockery = require('mockery');
+var Q = require('q');
+var assert = require('assert');
+var util = require('util');
+var path = require('path');
+var Emitter = require('events').EventEmitter;
+
+var mockClient;
+// used to test how many sounds were played by prompt module
+var soundsPlayed = 0;
+// milliseconds to delay async ops for mock requests
+var asyncDelay = 100;
+var mockeryOpts = {
+  warnOnReplace: false,
+  warnOnUnregistered: false,
+  useCleanCache: true
+};
+
+/**
+ * Returns a mock client that also acts as a Channel and Playback instance to
+ * allow a single EventEmitter to be used for testing.
+ *
+ * The mock client is cached so tests can access it to emit events if
+ * necessary.
+ */
+var getMockClient = function() {
+
+  if (mockClient) {
+    return mockClient;
+  }
+
+  var Client = function() {
+    this.Playback = function() {
+      return this;
+    };
+
+    this.getChannel = function() {
+      return this;
+    };
+
+    // actually channel.play
+    this.play = function() {
+      var self = this;
+      var innerDeferred = Q.defer();
+
+      setTimeout(function() {
+        innerDeferred.resolve();
+        soundsPlayed += 1;
+        self.emit('PlaybackFinished');
+      }, asyncDelay);
+
+      return innerDeferred.promise;
+    };
+
+    // actually playback.stop
+    this.stop = function() {
+      var innerDeferred = Q.defer();
+
+      setTimeout(function() {
+        innerDeferred.resolve();
+      }, asyncDelay);
+
+      return innerDeferred.promise;
+    };
+  };
+  util.inherits(Client, Emitter);
+
+  mockClient = new Client();
+
+  return mockClient;
+};
+
+/**
+ * Returns a mock app config for testing.
+ */
+var getMockAppConfig = function() {
+  var config = {
+    ari: {
+      url: '',
+      username: '',
+      password: '',
+      applicationName: ''
+    }
+  };
+
+  return config;
+};
+
+describe('prompt', function() {
+
+  beforeEach(function(done) {
+
+    mockery.enable(mockeryOpts);
+
+    var clientMock = {
+      getClient: function(url, username, password, appName) {
+        var deferred = Q.defer();
+        deferred.resolve(getMockClient());
+
+        return deferred.promise;
+      }
+    };
+    mockery.registerMock('ari-client-wrapper', clientMock);
+
+    var configMock = getMockAppConfig();
+    mockery.registerMock(path.resolve('./config.json'), configMock);
+
+    done();
+  });
+
+  afterEach(function(done) {
+    mockery.disable();
+    soundsPlayed = 0;
+
+    done();
+  });
+
+  it('should support playing a sequence of sounds', function(done) {
+    var ari = require('ari-client-wrapper');
+    var promptHelper = require('../lib/prompt.js');
+
+    var channel = getMockClient().getChannel();
+    var sounds = [{
+      sound: 'sound:hello-world',
+      skipable: false,
+      postSilence: 1
+    }, {
+      sound: 'sound:hello-world',
+      skipable: true,
+      postSilence: 0
+    }];
+    var prompts = promptHelper.create(sounds, channel);
+
+    prompts.play()
+      .then(function(played) {
+        assert(played);
+        assert(soundsPlayed === 2);
+
+        done();
+      })
+      .done();
+  });
+
+  it('should support stopping a sequence of sounds', function(done) {
+    var ari = require('ari-client-wrapper');
+    var promptHelper = require('../lib/prompt.js');
+
+    var channel = getMockClient().getChannel();
+    var sounds = [{
+      sound: 'sound:hello-world',
+      skipable: false,
+      postSilence: 1
+    }, {
+      sound: 'sound:hello-world',
+      skipable: true,
+      postSilence: 0
+    }];
+    var prompts = promptHelper.create(sounds, channel);
+
+    prompts.play()
+      .then(function(played) {
+        assert(!played);
+        assert(soundsPlayed < 2);
+
+        done();
+      })
+      .done();
+    prompts.stop();
+  });
+
+  it('should handle channel hanging up', function(done) {
+    var ari = require('ari-client-wrapper');
+    var promptHelper = require('../lib/prompt.js');
+
+    var channel = getMockClient().getChannel();
+    var sounds = [{
+      sound: 'sound:hello-world',
+      skipable: false,
+      postSilence: 1
+    }, {
+      sound: 'sound:hello-world',
+      skipable: true,
+      postSilence: 0
+    }];
+    var prompts = promptHelper.create(sounds, channel);
+
+    prompts.play()
+      .catch(function(err) {
+        assert(soundsPlayed < 2);
+        assert(~err.toString().match(/Channel hungup\.$/));
+
+        done();
+      })
+      .done();
+    getMockClient().emit('StasisEnd', {});
+  });
+
+});
